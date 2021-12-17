05af86e177b8b20b5822cec391e7a276091335fd
KunwarRajveerSingh <73644014+KunwarRajveerSingh@users.noreply.github.com>
2021-12-17 21:03:02 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,28 @@
+#Code for addition 
+n1 = float(input('Enter first number : '))
+n2 = float(input('Enter second number : '))
+
+sum = n1 + n2
+print('Sum =', sum)
+
+#Code for multiplication 
+n1 = float(input('Enter first number : '))
+n2 = float(input('Enter second number : '))
+
+sum = n1 * n2
+print('Sum =', sum)
+
+#Code for subtraction
+n1 = float(input('Enter first number : '))
+n2 = float(input('Enter second number : '))
+
+sum = n1 - n2
+print('Sum =', sum)
+
+#Code for division 
+n1 = float(input('Enter first number : '))
+n2 = float(input('Enter second number : '))
+
+sum = n1 / n2
+print('Sum =', sum)
+
